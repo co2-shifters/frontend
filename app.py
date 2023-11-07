a4c8037ed83401ad2de7699baf1df8c1b47c573c
Sven Chen <sven.chen@gmx.de>
2023-11-07 14:00:11 +0100

    plotting test data

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask.helpers import send_file
 import plotly.graph_objects as go
+from plotly.offline import plot
 
 # Create a Flask application
 app = Flask(__name__)
 
+# Static JSON data (this would normally come from a data source or be dynamically generated)
+data_json = {
+    "zone": "CH",
+    "forecast": [
+        {
+            "carbonIntensity": 90,
+            "datetime": "2023-11-01T07:00:00.000Z"
+        },
+        {
+            "carbonIntensity": 107,
+            "datetime": "2023-11-01T08:00:00.000Z"
+        }
+    ],
+    "optimisation": [
+        {
+            "rank": 1,
+            "optimal_start_time": "2023-11-01T09:00:00.000Z",
+            "co2_consumption": 90,
+            "co2_saving_percentage_max": 30
+        }
+    ]
+}
 
 # Define a route for the index page
 @app.route('/')
@@ -12,18 +35,38 @@ def indexPage():
     return send_file("templates/index.html")
 
 
-# Define a route for the results page
 @app.route('/results', methods=['GET'])
 def resultsPage():
+    # Extract data for plotting
+    dates = [entry["datetime"] for entry in data_json["forecast"]]
+    carbon_intensity = [entry["carbonIntensity"] for entry in data_json["forecast"]]
 
-    # Create the plot
-    fig = go.Figure(data=go.Scatter(x=[1, 2, 3, 4], y=[10, 11, 12, 13], mode='markers'))
+    # Create a scatter plot for carbon intensity forecast
+    trace_forecast = go.Scatter(
+        x=dates,
+        y=carbon_intensity,
+        mode='lines+markers',
+        name='Carbon Intensity Forecast'
+    )
 
-    # Render the plot as HTML
-    plot_html = fig.to_html(full_html=False, default_height=500, default_width=700)
+    # If you want to include optimisation data as well, do the same extraction for it
+    # ...
 
-    # Render the results template with the plot
-    return render_template('results.html', plot_html=plot_html)
+    # Define the layout of the plot
+    layout = go.Layout(
+        title='CO2 Consumption Forecast',
+        xaxis=dict(title='Date and Time'),
+        yaxis=dict(title='Carbon Intensity (gCO2/kWh)')
+    )
+
+    # Create the figure with the data and layout
+    fig = go.Figure(data=[trace_forecast], layout=layout)
+
+    # Convert the figure to HTML div element
+    plot_div = plot(fig, output_type='div', include_plotlyjs=True)
+
+    # Render the results template with the plot div
+    return render_template('results.html', plot_html=plot_div)
 
 
 if __name__ == '__main__':
