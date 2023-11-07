cff4a8ebc292ee841d370211b9d61e2f06bb8505
noahstammm <stammnoa@students.zhaw.ch>
2023-11-07 11:59:34 +0100

    updated app.py

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ import plotly.graph_objects as go
 # Create a Flask application
 app = Flask(__name__)
 
+
 # Define a route for the index page
 @app.route('/')
 def indexPage():
     return send_file("templates/index.html")
 
+
 # Define a route for the results page
-@app.route('/results')
+@app.route('/results', methods=['GET'])
 def resultsPage():
+
     # Create the plot
     fig = go.Figure(data=go.Scatter(x=[1, 2, 3, 4], y=[10, 11, 12, 13], mode='markers'))
 
@@ -21,6 +24,7 @@ def resultsPage():
 
     # Render the results template with the plot
     return render_template('results.html', plot_html=plot_html)
+
 
 if __name__ == '__main__':
     # Run the application on port 5000
